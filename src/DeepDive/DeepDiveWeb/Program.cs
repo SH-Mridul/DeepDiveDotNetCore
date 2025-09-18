05b3363a9fc602a2aaf4f982e84aae783e87b50e
mridulhossain <mridulcse8@gmail.com>
2025-09-19 01:55:11 +0600

    experiment with get,put,post,delete method in http request.

--- src/DeepDive/DeepDiveWeb/Program.cs
+++ src/DeepDive/DeepDiveWeb/Program.cs
@@ -11,7 +11,28 @@ namespace DeepDiveWeb
 
             app.Run(async (HttpContext context) =>
             {
-                if(context.Request.Method == "GET")
+                if (context.Request.Method == "GET" && context.Request.Path.StartsWithSegments("/employees") && context.Request.Query.Any())
+                {
+                    if (context.Request.Query.ContainsKey("id"))
+                    {
+                        int employeeId;
+                        var id = int.TryParse(context.Request.Query["id"],out employeeId);
+                        if (EmployeesRepository.CheckEmployeeExist(employeeId))
+                        {
+                            var employee = EmployeesRepository.GetEmployeeById(employeeId);
+                            await context.Response.WriteAsync($"id:{employee.Id}|name:{employee.Name}|salary:{employee.Salary}");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync($"Not found any employee by this id!");
+                        }
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync($"Bad Request!");
+                    }
+                }
+                else if (context.Request.Method == "GET")
                 {
                     if (context.Request.Path.StartsWithSegments("/"))
                     {
@@ -29,7 +50,7 @@ namespace DeepDiveWeb
                         var employees = EmployeesRepository.GetEmployees();
                         foreach (var employee in employees)
                         {
-                            await context.Response.WriteAsync($"{employee.Name}:{employee.Salary}\n");
+                            await context.Response.WriteAsync($"Id:{employee.Id}|Name:{employee.Name}|Salary:{employee.Salary}\n");
                         }
                     }
                 }
@@ -41,6 +62,47 @@ namespace DeepDiveWeb
                         var body = await reader.ReadToEndAsync();
                         var employee = JsonSerializer.Deserialize<Employee>(body);
                         EmployeesRepository.AddEmployee(employee);
+                    }
+                }
+                else if (context.Request.Method == "PUT")
+                {
+                    if (context.Request.Path.StartsWithSegments("/updateEmployee"))
+                    {
+                        using var reader = new StreamReader(context.Request.Body);
+                        var body = await reader.ReadToEndAsync();
+                        var employee = JsonSerializer.Deserialize<Employee>(body);
+                        var result = EmployeesRepository.UpdateEmployee(employee);
+                        if (result)
+                            await context.Response.WriteAsync("Employee is updated!");
+                        else
+                            await context.Response.WriteAsync("Employee not found!");
+
+                    }
+                }
+                else if(context.Request.Method == "DELETE" && context.Request.Path.StartsWithSegments("/employees") && context.Request.Query.Any())
+                {
+                    if(context.Request.Query.ContainsKey("id"))
+                    {
+
+                        int employeeId;
+                        var id = int.TryParse(context.Request.Query["id"], out employeeId);
+                        if(EmployeesRepository.CheckEmployeeExist(employeeId))
+                        {
+                            var employee = EmployeesRepository.GetEmployeeById(employeeId);
+                            var result = EmployeesRepository.Remove(employee);
+                            if (result)
+                                await context.Response.WriteAsync("Employee removed successfully!");
+                            else
+                                await context.Response.WriteAsync("something went wrong when employee removing!");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync("employee not exist by this id!");
+                        }
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Bad Request!");
                     }
                 }
             });
@@ -79,6 +141,40 @@ namespace DeepDiveWeb
         public static void AddEmployee(Employee? employee){
             if (employee is not null)
                 _employees.Add(employee);
+        }
+
+        public static bool UpdateEmployee(Employee? employee)
+        {
+            if (employee is not null)
+            {
+                var oldEmployee = _employees.FirstOrDefault(e => e.Id == employee.Id);
+                if (oldEmployee is not null)
+                {
+                    oldEmployee.Salary = employee.Salary;
+                    oldEmployee.Name = employee.Name;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static Employee GetEmployeeById(int id)
+        {
+            return _employees.FirstOrDefault( e => e.Id == id);
+        }
+
+        public static bool CheckEmployeeExist(int id)
+        {
+            return _employees.Exists(x => x.Id == id);
+        }
+
+        public static bool Remove(Employee employee)
+        {
+            if (_employees.Remove(employee))
+                return true;
+            return false;   
+            
         }
 
     }
